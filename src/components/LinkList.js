fbd97f0279f0c306a9566ea29c2a7e8d42f95d69
Lian Thompson <lianthompson@Lians-MacBook-Pro-2.local>
2018-08-12 20:52:54 -0700

    implement subscribe to new links function

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -42,9 +42,23 @@ class LinkList extends Component {
         store.writeQuery({ query: FEED_QUERY, data })
       }
 
-    _subscribeToNewLinks = async () => {
-        // implement soon
-    }
+      _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+          document: NEW_LINKS_SUBSCRIPTION,
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev
+            const newLink = subscriptionData.data.newLink.node
+      
+            return Object.assign({}, prev, {
+              feed: {
+                links: [newLink, ...prev.feed.links],
+                count: prev.feed.links.length + 1,
+                __typename: prev.feed.__typename
+              }
+            })
+          }
+        })
+      }
 
     render() {
         return (
